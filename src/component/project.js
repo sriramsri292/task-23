1a2982004c2e910f068adc8c8a23857066f3c230
sriramsri292 <133184406+sriramsri292@users.noreply.github.com>
2023-07-16 14:53:32 +0530

    Update project.js

--- src/component/project.js
+++ src/component/project.js
@@ -60,67 +60,97 @@ const Shop = () => {
       image: "https://dummyimage.com/450x300/dee2e6/6c757d.jpg",
     },
     {
-        id: 2,
-        name: "Special Item",
-        price: "$20.00 $18.00",
-        image: "https://dummyimage.com/450x300/dee2e6/6c757d.jpg",
-      },{
-        id: 3,
-        name: "Sale Item",
-        price: "$50.00 $25.00",
-        image: "https://dummyimage.com/450x300/dee2e6/6c757d.jpg",
-      },{
-        id: 4,
-        name: "Popular Item",
-        price: "$40.00",
-        image: "https://dummyimage.com/450x300/dee2e6/6c757d.jpg",
-      },{
-        id: 5,
-        name: "Sale Item",
-        price: "$50.00 $25.00",
-        image: "https://dummyimage.com/450x300/dee2e6/6c757d.jpg",
-      },
-      {
-        id: 6,
-        name: "Fancy Product",
-        price: "$120.00 - $280.00",
-        image: "https://dummyimage.com/450x300/dee2e6/6c757d.jpg",
-      },
-      {
-        id: 7,
-        name: "Special Item",
-        price: "$20.00 $18.00",
-        image: "https://dummyimage.com/450x300/dee2e6/6c757d.jpg",
-      },
-      
-
+      id: 3,
+      name: "Sale Item",
+      price: "$50.00 $25.00",
+      image: "https://dummyimage.com/450x300/dee2e6/6c757d.jpg",
+    },
+    {
+      id: 4,
+      name: "Popular Item",
+      price: "$40.00",
+      image: "https://dummyimage.com/450x300/dee2e6/6c757d.jpg",
+    },
+    {
+      id: 5,
+      name: "Sale Item",
+      price: "$50.00 $25.00",
+      image: "https://dummyimage.com/450x300/dee2e6/6c757d.jpg",
+    },
+    {
+      id: 6,
+      name: "Fancy Product",
+      price: "$120.00 - $280.00",
+      image: "https://dummyimage.com/450x300/dee2e6/6c757d.jpg",
+    },
+    {
+      id: 7,
+      name: "Special Item",
+      price: "$20.00 $18.00",
+      image: "https://dummyimage.com/450x300/dee2e6/6c757d.jpg",
+    },
   ];
 
   return (
     <div>
       <nav className="navbar navbar-expand-lg navbar-light bg-light">
         <div className="container px-4 px-lg-5">
-          <a className="navbar-brand" href="#!">Start Bootstrap</a>
-          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <a className="navbar-brand" href="#!">
+            Start Bootstrap
+          </a>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
             <span className="navbar-toggler-icon"></span>
           </button>
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <ul className="navbar-nav me-auto mb-2 mb-lg-0 ms-lg-4">
               <li className="nav-item">
-                <a className="nav-link active" aria-current="page" href="#!">Home</a>
+                <a className="nav-link active" aria-current="page" href="#!">
+                  Home
+                </a>
               </li>
               <li className="nav-item">
-                <a className="nav-link" href="#!">About</a>
+                <a className="nav-link" href="#!">
+                  About
+                </a>
               </li>
               <li className="nav-item dropdown">
-                <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                <a
+                  className="nav-link dropdown-toggle"
+                  href="#"
+                  id="navbarDropdown"
+                  role="button"
+                  data-bs-toggle="dropdown"
+                  aria-expanded="false"
+                >
                   Shop
                 </a>
                 <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
-                  <li><a className="dropdown-item" href="#!">All Products</a></li>
-                  <li><hr className="dropdown-divider" /></li>
-                  <li><a className="dropdown-item" href="#!">Popular Items</a></li>
-                  <li><a className="dropdown-item" href="#!">New Arrivals</a></li>
+                  <li>
+                    <a className="dropdown-item" href="#!">
+                      All Products
+                    </a>
+                  </li>
+                  <li>
+                    <hr className="dropdown-divider" />
+                  </li>
+                  <li>
+                    <a className="dropdown-item" href="#!">
+                      Popular Items
+                    </a>
+                  </li>
+                  <li>
+                    <a className="dropdown-item" href="#!">
+                      New Arrivals
+                    </a>
+                  </li>
                 </ul>
               </li>
             </ul>
@@ -128,15 +158,15 @@ const Shop = () => {
               <button className="btn btn-outline-dark" type="submit">
                 <i className="bi-cart-fill me-1"></i>
                 Cart
-                <span className="badge bg-dark text-white ms-1 rounded-pill">{cart.length}</span>
+                <span className="badge bg-dark text-white ms-1 rounded-pill">
+                  {cart.length}
+                </span>
               </button>
             </form>
           </div>
         </div>
       </nav>
-      <header className="bg-dark py-5">
-        {/* Header content */}
-      </header>
+      <header className="bg-dark py-5">{/* Header content */}</header>
       <section className="py-5">
         <div className="container px-4 px-lg-5 mt-5">
           <div className="row gx-4 gx-lg-5 row-cols-2 row-cols-md-3 row-cols-xl-4 justify-content-center">
@@ -144,9 +174,7 @@ const Shop = () => {
           </div>
         </div>
       </section>
-      <footer className="py-5 bg-dark">
-        {/* Footer content */}
-      </footer>
+      <footer className="py-5 bg-dark">{/* Footer content */}</footer>
     </div>
   );
 };
